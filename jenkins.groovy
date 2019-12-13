8643ea9a291e56015bfc0fc3abc45dff21c5e3e0
Arshad Shah <arshad.shah@philips.com>
2019-12-13 19:22:26 +0530

    Added checkstyle

--- jenkins.groovy
+++ jenkins.groovy
@@ -13,11 +13,7 @@ node {
             }
 
             stage("Staging") {
-                sh "pid=\$(lsof -i:8989 -t); kill -TERM \$pid "
-                + "|| kill -KILL \$pid"
-                withEnv(['JENKINS_NODE_COOKIE=dontkill']) {
                     sh './mvnw spring-boot:run -Dserver.port=8989 &'
                 }
-            }
         }
     }
