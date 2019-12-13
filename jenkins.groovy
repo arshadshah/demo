0026dc0f4ff61faf01a8ac0167559b29b93890e2
Arshad Shah <arshad.shah@philips.com>
2019-12-13 18:51:19 +0530

    Added checkstyle

--- jenkins.groovy
+++ jenkins.groovy
@@ -3,24 +3,6 @@ node {
     git 'https://github.com/arshadshah/demo.git'
 
     dir('spring-jenkins-pipeline') {
-        stage("Compilation and Analysis") {
-            parallel 'Compilation': {
-                sh "./mvnw clean install -DskipTests"
-            }, 'Static Analysis': {
-                stage("Checkstyle") {
-                    sh "./mvnw checkstyle:checkstyle"
-
-                    step([$class: 'CheckStylePublisher',
-                          canRunOnFailed: true,
-                          defaultEncoding: '',
-                          healthy: '100',
-                          pattern: '**/target/checkstyle-result.xml',
-                          unHealthy: '90',
-                          useStableBuildAsReference: true
-                    ])
-                }
-            }
-        }
 
         stage("Tests and Deployment") {
             parallel 'Unit tests': {
