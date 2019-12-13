14d1c41a5c0f1ed67aece5248ab463b5c853ec77
Arshad Shah <arshad.shah@philips.com>
2019-12-13 19:11:59 +0530

    Added checkstyle

--- jenkins.groovy
+++ jenkins.groovy
@@ -7,30 +7,9 @@ node {
         stage("Tests and Deployment") {
             parallel 'Unit tests': {
                 stage("Runing unit tests") {
-                    try {
-                        sh "./mvnw test -Punit"
-                    } catch(err) {
-                        step([$class: 'JUnitResultArchiver', testResults:
-                            '**/target/surefire-reports/TEST-*UnitTest.xml'])
-                        throw err
-                    }
-                    step([$class: 'JUnitResultArchiver', testResults:
-                        '**/target/surefire-reports/TEST-*UnitTest.xml'])
+                        sh "./mvnw test"
                 }
-            }, 'Integration tests': {
-                stage("Runing integration tests") {
-                    try {
-                        sh "./mvnw test -Pintegration"
-                    } catch(err) {
-                        step([$class: 'JUnitResultArchiver', testResults:
-                            '**/target/surefire-reports/TEST-'
-                                + '*IntegrationTest.xml'])
-                        throw err
-                    }
-                    step([$class: 'JUnitResultArchiver', testResults:
-                        '**/target/surefire-reports/TEST-'
-                            + '*IntegrationTest.xml'])
-                }
+            }
             }
 
             stage("Staging") {
